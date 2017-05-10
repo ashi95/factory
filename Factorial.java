a2e0c03f17b9556b28c01e1ff3d68048a65db85c
ashi95 <khalkhoreda@gmail.com>
2017-05-10 16:36:19 +0200

    first commit

---
+++ Factorial.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alucard
+ */
+public class Factorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        System.out.println("enter a number");
+        number=sc.nextInt();
+        System.out.println(factorial(number));
+        
+    }
+    public static int factorial(int num)
+    {
+        if(num==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return num*factorial(num-1);
+        }
+    }
+}
